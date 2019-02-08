01418546238a5b103412cf418e63b737a91b600e
Christophe Rosset <tophe@topheman.com>
2019-02-08 11:07:32 +0100

    feat(api): add credits/translations by default to movie() call

--- src/@types/tmdb.d.ts
+++ src/@types/tmdb.d.ts
@@ -1,5 +1,5 @@
 /**
- * Generated with https://jvilk.com/MakeTypes/
+ * Based on generated types made with https://jvilk.com/MakeTypes/
  */
 export interface TmdbMovieEntity {
   adult: boolean;
@@ -27,6 +27,8 @@ export interface TmdbMovieEntity {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  translations?: TmdbTranslations;
+  credits?: TmdbCredits;
 }
 export interface TmdbGenresEntity {
   id: number;
@@ -45,4 +47,49 @@ export interface TmdbProductionCountriesEntity {
 export interface TmdbSpokenLanguagesEntity {
   iso_639_1: string;
   name: string;
+}
+export interface TmdbTranslations {
+  translations?: (TmdbTranslationEntity)[] | null;
+}
+export interface TmdbTranslationsEntity extends TmdbTranslations {
+  id: number;
+}
+export interface TmdbTranslationEntity {
+  iso_3166_1: string;
+  iso_639_1: string;
+  name: string;
+  english_name: string;
+  data: TranslationsEntityData;
+}
+export interface TmdbTranslationEntityData {
+  name?: string;
+  title?: string;
+  overview: string;
+  homepage: string;
+}
+export interface TmdbCredits {
+  cast?: (TmdbCastEntity)[] | null;
+  crew?: (TmdbCrewEntity)[] | null;
+}
+export interface TmdbCreditsEntity extends TmdbCredits {
+  id: number;
+}
+export interface TmdbCastEntity {
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  gender: number;
+  id: number;
+  name: string;
+  order: number;
+  profile_path?: string | null;
+}
+export interface TmdbCrewEntity {
+  credit_id: string;
+  department: string;
+  gender: number;
+  id: number;
+  job: string;
+  name: string;
+  profile_path?: string | null;
 }
--- src/services/apis/decorateApis/tmdb.ts
+++ src/services/apis/decorateApis/tmdb.ts
@@ -4,7 +4,7 @@ import { TmdbMovieEntity } from "../../../@types";
 export interface TmdbDecorateAPI {
   movie: (
     id: number | string,
-    { language }: { language: string }
+    { language, append }: { language: string; append?: string[] }
   ) => Promise<TmdbMovieEntity>;
 }
 
@@ -17,12 +17,13 @@ const decorateApi = ({
     api_key: process.env.NEXTJS_APP_CLIENT_TMDB_API_KEY
   };
   return {
-    movie: (id, { language }) => {
+    movie: (id, { language, append = ["credits", "translations"] }) => {
       const query = `/movie/${id}`;
       return client
         .get(query, {
           params: {
-            language: language || "en"
+            language: language || "en",
+            append_to_response: append.join(",")
           }
         })
         .then(({ data }) => data);

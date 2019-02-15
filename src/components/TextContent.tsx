8fa4d641065d768a08085cf712af1f22a565d4b2
Christophe Rosset <tophe@topheman.com>
2019-02-15 08:15:21 +0100

    test(TextContent)

--- src/components/TextContent.tsx
+++ src/components/TextContent.tsx
@@ -1,3 +1,7 @@
+/**
+ * Takes a string as children
+ * Return ONE or MULTIPLE <p> tags split inside from the \n\n
+ */
 const TextContent: React.SFC<
   { children: string } & React.HTMLAttributes<HTMLElement>
 > = ({ children, ...props }) => (

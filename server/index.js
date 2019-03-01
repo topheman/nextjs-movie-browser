b91394d4aeb4a14b8a0746eeb179dcf9587b0bbc
Christophe Rosset <tophe@topheman.com>
2019-03-01 19:19:37 +0100

    chore(*): add log infos at server startup

--- server/index.js
+++ server/index.js
@@ -124,13 +124,16 @@ app.prepare().then(() => {
   server.listen(port, err => {
     if (err) throw err;
     console.log(
-      `> Ready on http://localhost:${port} - mode: ${
-        process.env.NODE_ENV
-      } - with NEXTJS_APP_CLIENT_BASE_PATH${
+      `> Ready on http://localhost:${port} - mode: ${process.env.NODE_ENV}`
+    );
+    console.log(
+      `>   - NEXTJS_APP_CLIENT_BASE_PATH${
         process.env.NEXTJS_APP_CLIENT_BASE_PATH
           ? `=${process.env.NEXTJS_APP_CLIENT_BASE_PATH}`
           : " not set, will use hostname"
       }`
     );
+    console.log(`>   - RECORD_MOCKS=${process.env.RECORD_MOCKS}`);
+    console.log(`>   - MOCKS_ENABLED=${process.env.MOCKS_ENABLED}`);
   });
 });

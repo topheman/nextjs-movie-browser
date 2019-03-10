68f292ec03efd63bab0c2dc3dc31b9091f46ea7c
Christophe Rosset <tophe@topheman.com>
2019-03-10 09:01:51 +0100

    feat(analytics): setup google analytics
    https://www.garymeehan.ie/blog/google-analytics-nextjs-and-prismic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,12 +13,14 @@ import { getBasePath } from "../src/utils/url";
 import DocumentLinkTags from "../src/components/DocumentLinkTags";
 
 export default class MyDocument extends Document<{
+  isProduction: boolean;
   defaultLanguageShortCode: string;
   defaultLanguageFullCode: string;
   basePath: string;
 }> {
   static async getInitialProps(ctx: NextDocumentContext) {
     console.log("MyDocument.getInitialProps");
+    const isProduction = process.env.NODE_ENV === "production";
     const initialProps = await Document.getInitialProps(ctx);
     const defaultLanguageShortCode = getDefaultLanguageFromCookie(
       ctx.req && ctx.req.headers.cookie
@@ -29,9 +31,27 @@ export default class MyDocument extends Document<{
     );
     return {
       ...initialProps,
+      isProduction,
       defaultLanguageShortCode,
       defaultLanguageFullCode,
       basePath: getBasePath(ctx.req as any, undefined)
+    };
+  }
+
+  // Function will be called below to inject
+  // script contents onto page
+  setGoogleTags() {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        if (location.hostname !== "localhost") {
+          gtag('js', new Date());
+          gtag('config', '${
+            process.env.NEXTJS_APP_CLIENT_GOOGLE_ANALYTICS_ID
+          }');
+        }
+      `
     };
   }
 
@@ -53,6 +73,17 @@ export default class MyDocument extends Document<{
             <Main />
             <div id="modal" />
             <NextScript />
+            {this.props.isProduction && (
+              <>
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${
+                    process.env.NEXTJS_APP_CLIENT_GOOGLE_ANALYTICS_ID
+                  }`}
+                />
+                <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+              </>
+            )}
           </body>
         </html>
         <script

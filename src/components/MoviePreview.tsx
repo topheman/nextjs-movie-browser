c717d95b17b24948ac2d2aca1f4e25682f768e4b
Christophe Rosset <tophe@topheman.com>
2019-03-15 22:39:07 +0100

    feat(MoviePreview): start styling

--- src/components/MoviePreview.tsx
+++ src/components/MoviePreview.tsx
@@ -1,16 +1,69 @@
 import React from "react";
 import i18next from "i18next";
+import styled from "styled-components";
 
 import { withNamespaces } from "../../i18n";
 import { TmdbMovieEntity, ComponentWithData, TmdbTvEntity } from "../@types";
 import TextContent from "./TextContent";
 import Link from "./Link";
+import { makeImageTmdbUrl } from "../utils/tmdb";
 
 interface IProps extends ComponentWithData<TmdbMovieEntity & TmdbTvEntity> {
   t: i18next.TranslationFunction;
   mode: "preview" | "full";
   media_type: "movie" | "tv";
 }
+
+const Wrapper = styled.div`
+  top: 0;
+  left: 0;
+  padding: 0;
+  background-color: ${props => props.theme.primary}40;
+  section {
+    display: grid;
+    grid-template-columns: 300px 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "poster title"
+      "poster content"
+      "poster .";
+    padding: 20px 0px;
+    margin: 0 auto;
+    max-width: ${props => props.theme.maxWidth};
+  }
+  @media screen and (max-width: ${props => props.theme.breakpoint}) {
+    section {
+      padding: 1px 8px;
+    }
+  }
+`;
+
+const Poster = styled.div<{ poster_path: string }>`
+  grid-area: poster;
+  width: 300px;
+  height: 450px;
+  display: block;
+  background-image: url(${props =>
+    makeImageTmdbUrl(props.poster_path, "w300")});
+  @media screen and (max-width: ${props => props.theme.breakpoint}) {
+    width: 200px;
+    height: 300px;
+    background-image: url(${props =>
+      makeImageTmdbUrl(props.poster_path, "w200")});
+  }
+`;
+
+const Title = styled.h2`
+  grid-area: title;
+  margin: 0px;
+  padding: 0 8px 0 20px;
+`;
+
+const Content = styled.div`
+  grid-area: content;
+  margin: 0px;
+  padding: 0 8px 0 20px;
+`;
 
 /**
  * Preview for Movie and Tv
@@ -22,26 +75,33 @@ const MoviePreview: React.FunctionComponent<IProps> = ({
   data
 }) => {
   return (
-    <>
-      <h2 dir="auto" data-testid="movie-preview-title">
-        {mode === "preview" && (
-          <Link tmdbEntity={{ ...data, media_type }}>
-            <a>👈</a>
-          </Link>
-        )}
-        {(data as TmdbMovieEntity).title || (data as TmdbTvEntity).name}{" "}
-        {(data as TmdbMovieEntity).release_date &&
-          `(${new Date((data as TmdbMovieEntity).release_date).getFullYear()})`}
-      </h2>
-      {mode === "full" && (
-        <>
-          <h3 data-testid="movie-preview-title-synopsis">
-            {t("movie-label-synopsis")}
-          </h3>
-          <TextContent>{data.overview}</TextContent>
-        </>
-      )}
-    </>
+    <Wrapper>
+      <section>
+        <Poster poster_path={data.poster_path} />
+        <Title dir="auto" data-testid="movie-preview-title">
+          {mode === "preview" && (
+            <Link tmdbEntity={{ ...data, media_type }}>
+              <a>👈</a>
+            </Link>
+          )}
+          {(data as TmdbMovieEntity).title || (data as TmdbTvEntity).name}{" "}
+          {(data as TmdbMovieEntity).release_date &&
+            `(${new Date(
+              (data as TmdbMovieEntity).release_date
+            ).getFullYear()})`}
+        </Title>
+        <Content>
+          {mode === "full" && (
+            <>
+              <h3 data-testid="movie-preview-title-synopsis">
+                {t("movie-label-synopsis")}
+              </h3>
+              <TextContent>{data.overview}</TextContent>
+            </>
+          )}
+        </Content>
+      </section>
+    </Wrapper>
   );
 };
 

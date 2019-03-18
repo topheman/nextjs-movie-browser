cdb3e3279585f66ae8daafb8c25cb0eaa0eab8e1
Christophe Rosset <tophe@topheman.com>
2019-03-18 18:09:29 +0100

    feat(*): design /cast and /seasons pages

--- src/components/MovieCast.tsx
+++ src/components/MovieCast.tsx
@@ -3,6 +3,7 @@ import i18next from "i18next";
 
 import { withNamespaces } from "../../i18n";
 import { TmdbMovieEntity, TmdbTvEntity, ComponentWithData } from "../@types";
+import { MainWrapper } from "./ui/Layout";
 import Link from "./Link";
 
 interface IProps extends ComponentWithData<TmdbMovieEntity & TmdbTvEntity> {
@@ -21,57 +22,59 @@ const MovieCast: React.FunctionComponent<IProps> = ({
   data
 }) => {
   return (
-    <>
-      {data.credits.cast && data.credits.cast.length > 0 && (
-        <>
-          <h3>{t("movie-label-cast")}</h3>
-          <ul>
-            {data.credits.cast
-              .filter((_, index) => {
-                if (mode === "full" || (mode === "preview" && index < 5)) {
-                  return true;
-                }
-                return false;
-              })
-              .map(person => (
-                <li key={person.credit_id}>
-                  <Link tmdbEntity={{ media_type: "person", ...person }}>
-                    <a>{person.name}</a>
-                  </Link>{" "}
-                  <i>{person.character}</i>
-                </li>
-              ))}
-          </ul>
-        </>
-      )}
-      {data.credits.crew && data.credits.crew.length > 0 && (
-        <>
-          <h3>{t("movie-label-crew")}</h3>
-          <ul>
-            {data.credits.crew
-              .filter((_, index) => {
-                if (mode === "full" || (mode === "preview" && index < 5)) {
-                  return true;
-                }
-                return false;
-              })
-              .map(person => (
-                <li key={person.credit_id}>
-                  <Link tmdbEntity={{ media_type: "person", ...person }}>
-                    <a>{person.name}</a>
-                  </Link>{" "}
-                  {person.job} <i>({person.department})</i>
-                </li>
-              ))}
-          </ul>
-        </>
-      )}
-      {mode === "preview" && (
-        <Link tmdbEntity={{ media_type, ...data }} subcategory="cast">
-          <a>{t("movie-label-full-casting")}</a>
-        </Link>
-      )}
-    </>
+    <MainWrapper>
+      <section>
+        {data.credits.cast && data.credits.cast.length > 0 && (
+          <>
+            <h3>{t("movie-label-cast")}</h3>
+            <ul>
+              {data.credits.cast
+                .filter((_, index) => {
+                  if (mode === "full" || (mode === "preview" && index < 5)) {
+                    return true;
+                  }
+                  return false;
+                })
+                .map(person => (
+                  <li key={person.credit_id}>
+                    <Link tmdbEntity={{ media_type: "person", ...person }}>
+                      <a>{person.name}</a>
+                    </Link>{" "}
+                    <i>{person.character}</i>
+                  </li>
+                ))}
+            </ul>
+          </>
+        )}
+        {data.credits.crew && data.credits.crew.length > 0 && (
+          <>
+            <h3>{t("movie-label-crew")}</h3>
+            <ul>
+              {data.credits.crew
+                .filter((_, index) => {
+                  if (mode === "full" || (mode === "preview" && index < 5)) {
+                    return true;
+                  }
+                  return false;
+                })
+                .map(person => (
+                  <li key={person.credit_id}>
+                    <Link tmdbEntity={{ media_type: "person", ...person }}>
+                      <a>{person.name}</a>
+                    </Link>{" "}
+                    {person.job} <i>({person.department})</i>
+                  </li>
+                ))}
+            </ul>
+          </>
+        )}
+        {mode === "preview" && (
+          <Link tmdbEntity={{ media_type, ...data }} subcategory="cast">
+            <a>{t("movie-label-full-casting")}</a>
+          </Link>
+        )}
+      </section>
+    </MainWrapper>
   );
 };
 
--- src/components/TvSeasons.tsx
+++ src/components/TvSeasons.tsx
@@ -1,39 +1,48 @@
 import React from "react";
 import i18next from "i18next";
+import styled from "styled-components";
 
 import { withNamespaces } from "../../i18n";
 import { TmdbTvEntity, ComponentWithData } from "../@types";
+import { MainWrapper } from "./ui/Layout";
 import TextContent from "./TextContent";
 
 interface IProps extends ComponentWithData<TmdbTvEntity> {
   t: i18next.TranslationFunction;
 }
 
+const SeasonList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
 const TvSeasons: React.FunctionComponent<IProps> = ({ t, data }) => {
   return (
-    <>
-      {data.seasons && data.seasons.length > 0 && (
-        <ul>
-          {data.seasons.map(season => (
-            <li key={season.id}>
-              <h3>
-                <span dir="auto">{season.name}</span>{" "}
-                <span style={{ fontSize: "0.8em" }}>
-                  {new Date(season.air_date).getFullYear()}
-                </span>
-                {" | "}
-                <span style={{ fontSize: "0.8em" }}>
-                  {t("movie-label-episode-count", {
-                    count: season.episode_count
-                  })}
-                </span>
-              </h3>
-              <TextContent>{season.overview}</TextContent>
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
+    <MainWrapper>
+      <section>
+        {data.seasons && data.seasons.length > 0 && (
+          <SeasonList>
+            {data.seasons.map(season => (
+              <li key={season.id}>
+                <h3>
+                  <span dir="auto">{season.name}</span>{" "}
+                  <span style={{ fontSize: "0.8em" }}>
+                    {new Date(season.air_date).getFullYear()}
+                  </span>
+                  {" | "}
+                  <span style={{ fontSize: "0.8em" }}>
+                    {t("movie-label-episode-count", {
+                      count: season.episode_count
+                    })}
+                  </span>
+                </h3>
+                <TextContent>{season.overview}</TextContent>
+              </li>
+            ))}
+          </SeasonList>
+        )}
+      </section>
+    </MainWrapper>
   );
 };
 
